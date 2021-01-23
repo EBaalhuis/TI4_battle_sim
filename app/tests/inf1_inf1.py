b8a7ff2e6eed78955087b407c71e1dd4d53b7588
Erik Baalhuis <ebbaalhuis@gmail.com>
2021-01-23 22:55:39 +0100

    test for 1 infantry vs 1 infantry

---
+++ app/tests/inf1_inf1.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from app import calculator
+
+# error tolerance in percentage point probability of each outcome
+TOL = 1
+TARGET = [18, 41, 41]
+
+attacker = defaultdict(lambda: 0)
+attacker["infantry"] = 1
+
+defender = defaultdict(lambda: 0)
+defender["infantry"] = 1
+
+options = defaultdict(lambda: 0)
+
+# Factions
+options["att_faction"] = "Winnu"
+options["def_faction"] = "Winnu"
+
+# Ground Combat
+options["ground_combat"] = True
+
+outcomes = calculator.calculate(attacker, defender, options)
+
+assert(abs(outcomes[0] - TARGET[0]) < TOL)
+assert(abs(outcomes[1] - TARGET[1]) < TOL)
+# assert(abs(outcomes[2] - TARGET[2]) < TOL)
+#
+# print("Attacker wins: %.1f%%" % outcomes[1])
+# print("Tie: %.1f%%" % outcomes[0])
+# print("Defender wins: %.1f%%" % outcomes[2])
