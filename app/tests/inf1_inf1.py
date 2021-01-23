92b89d2740ebe85b606d82dce786862d8cc12492
Erik Baalhuis <ebbaalhuis@gmail.com>
2021-01-24 00:00:10 +0100

    better prints

--- app/tests/inf1_inf1.py
+++ app/tests/inf1_inf1.py
@@ -1,12 +1,15 @@
+import os
+import sys
 from collections import defaultdict
-import sys, os
-
 sys.path.insert(1, os.path.join(sys.path[0], '../..'))
 from app import calculator
 
 # error tolerance in percentage point probability of each outcome
-TOL = 1
+TOL = 1.5
 TARGET = [18, 41, 41]
+
+file_name = __file__.split("/")[-1]
+print("Running test " + file_name)
 
 attacker = defaultdict(lambda: 0)
 attacker["infantry"] = 1
@@ -25,13 +28,11 @@ options["ground_combat"] = True
 
 outcomes = calculator.calculate(attacker, defender, options)
 
+print("Outcomes:")
 print(outcomes)
+print("Target:")
+print(TARGET)
 assert(abs(outcomes[0] - TARGET[0]) < TOL)
 assert(abs(outcomes[1] - TARGET[1]) < TOL)
 assert(abs(outcomes[2] - TARGET[2]) < TOL)
-
-print("Attacker wins: %.1f%%" % outcomes[1])
-print("Tie: %.1f%%" % outcomes[0])
-print("Defender wins: %.1f%%" % outcomes[2])
-file_name = __file__.split("/")[-1]
-print("Test " + file_name + " successful")
+print("Test successful\n")
