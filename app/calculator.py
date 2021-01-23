0192a8175c3c3a2aaa87ddcdfb97bdffc127d30a
Erik Baalhuis <ebbaalhuis@gmail.com>
2021-01-23 22:46:09 +0100

    implement Morale Boost

--- app/calculator.py
+++ app/calculator.py
@@ -12,19 +12,11 @@ def has_flagship(units):
     return len(list(filter(lambda x: x.name == "flagship", units))) > 0
 
 
-def generate_hits(units, faction, prototype, fire_team):
+def generate_hits(units, faction, morale, prototype, fire_team):
     result = 0
     for u in units:
         for val in u.combat:
             x = random.randint(1, 10)
-            if prototype and u.fighter:
-                x += 2
-            if x >= val:
-                result += 1
-            elif fire_team:  # re-roll if it was a miss, ground combat only (so no prototype)
-                x = random.randint(1, 10)
-                if x >= val:
-                    result += 1
 
             # Jol-Nar flagsip
             if faction == "Jol-Nar" and u.name == "flagship":
@@ -33,7 +25,25 @@ def generate_hits(units, faction, prototype, fire_team):
 
             # Sardakk flagship
             if faction == "Sardakk" and has_flagship(units) and u.name != "flagship":
-                if x == val - 1:
+                x += 1
+
+            # Morale Boost
+            if morale:
+                x += 1
+
+            # Prototype Fighter
+            if prototype and u.fighter:
+                x += 2
+
+            if x >= val:
+                result += 1
+
+            # Fire Team
+            elif fire_team:  # re-roll if it was a miss, ground combat only (so no prototype)
+                x = random.randint(1, 10)
+                if morale:
+                    x += 1
+                if x >= val:
                     result += 1
 
     return result
@@ -93,9 +103,13 @@ def combat_round(att_units, def_units, first_round, options):
     if options["att_faction"] == "Winnu" or options["def_faction"] == "Winnu":
         att_units, def_units = winnu_flagship(att_units, def_units, options)
 
-    att_hits = generate_hits(att_units, options["att_faction"], prototype=(first_round and options["att_prototype"]),
+    att_hits = generate_hits(att_units, options["att_faction"],
+                             morale=(first_round and options["att_morale"]),
+                             prototype=(first_round and options["att_prototype"]),
                              fire_team=(options["att_fireteam"] and first_round and options["ground_combat"]))
-    def_hits = generate_hits(def_units, options["def_faction"], prototype=(first_round and options["def_prototype"]),
+    def_hits = generate_hits(def_units, options["def_faction"],
+                             morale=(first_round and options["def_morale"]),
+                             prototype=(first_round and options["def_prototype"]),
                              fire_team=(options["def_fireteam"] and first_round and options["ground_combat"]))
 
     # Magen Defense Grid
