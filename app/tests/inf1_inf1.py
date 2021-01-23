c02f7c6da7859657538080e7edef55a48153a03f
Erik Baalhuis <ebbaalhuis@gmail.com>
2021-01-24 00:10:18 +0100

    clean up first test file

--- app/tests/inf1_inf1.py
+++ app/tests/inf1_inf1.py
@@ -4,20 +4,17 @@ from collections import defaultdict
 sys.path.insert(1, os.path.join(sys.path[0], '../..'))
 from app import calculator
 
-# error tolerance in percentage point probability of each outcome
-TOL = 1.5
-TARGET = [18, 41, 41]
 
-file_name = __file__.split("/")[-1]
-print("Running test " + file_name)
-
+TOL = 1.5  # error tolerance in percentage point probability of each outcome
+TARGET = [18, 41, 41]  # target percentages; [tie, attacker, defender]
 attacker = defaultdict(lambda: 0)
-attacker["infantry"] = 1
-
 defender = defaultdict(lambda: 0)
-defender["infantry"] = 1
-
 options = defaultdict(lambda: 0)
+
+
+# Units
+attacker["infantry"] = 1
+defender["infantry"] = 1
 
 # Factions
 options["att_faction"] = "Winnu"
@@ -26,8 +23,10 @@ options["def_faction"] = "Winnu"
 # Ground Combat
 options["ground_combat"] = True
 
-outcomes = calculator.calculate(attacker, defender, options)
+# Options
 
+
+outcomes = calculator.calculate(attacker, defender, options)
 print("Outcomes:")
 print(outcomes)
 print("Target:")
@@ -35,4 +34,4 @@ print(TARGET)
 assert(abs(outcomes[0] - TARGET[0]) < TOL)
 assert(abs(outcomes[1] - TARGET[1]) < TOL)
 assert(abs(outcomes[2] - TARGET[2]) < TOL)
-print("Test successful\n")
+print("Test successful")
