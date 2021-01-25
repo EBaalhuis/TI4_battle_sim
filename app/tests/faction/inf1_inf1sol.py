0d448d772339ed4c92a4e4631b6479e3717f678f
Erik Baalhuis <ebbaalhuis@gmail.com>
2021-01-25 17:45:06 +0100

    fix app import

--- app/tests/faction/inf1_inf1sol.py
+++ app/tests/faction/inf1_inf1sol.py
@@ -8,14 +8,15 @@ from app import testing_helpers
 attacker, defender, options, tol = testing_helpers.defaults()
 
 # target source: http://alphamou.se/ti4calc/
-target = [1, 1, 98]  # target percentages; [tie, attacker, defender]
-print("1 Infantry vs 3 Infantry")
+target = [21, 31, 48]  # target percentages; [tie, attacker, defender]
+print("1 Infantry vs 1 Infantry [Sol]")
 
 # Units
 attacker["infantry"] = 1
-defender["infantry"] = 3
+defender["infantry"] = 1
 
 # Factions
+options["def_faction"] = "Sol"
 
 # Ground Combat
 options["ground_combat"] = True
