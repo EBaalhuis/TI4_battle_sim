8adb1ba1248d29f2a56bc3e62cc36375630e42b1
Erik Baalhuis <ebbaalhuis@gmail.com>
2021-01-31 22:30:16 +0100

    test for Maneuvering Jets

---
+++ app/tests/action_cards/Maneuvering_Jets.py
@@ -0,0 +1,28 @@
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '../../..'))
+from app import calculator
+from app import testing_helpers
+
+
+attacker, defender, options, tol = testing_helpers.defaults()
+
+# target source: http://alphamou.se/ti4calc/
+target = [18, 41, 41]  # target percentages; [tie, attacker, defender]
+print("1 Infantry (Maneuvering Jets to cancel Space Cannon Defense) vs 1 Infantry 1 PDS")
+
+# Units
+attacker["infantry"] = 1
+defender["infantry"] = 1
+defender["pds"] = 1
+
+# Factions
+
+# Ground Combat
+options["ground_combat"] = True
+
+# Options
+options["att_maneuvering"] = True
+
+outcomes = calculator.calculate(attacker, defender, options)
+testing_helpers.evaluate(outcomes, target, tol)
