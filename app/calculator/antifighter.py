09957423ff9b7f420b425e036366f58e7c310719
Erik Baalhuis <ebbaalhuis@gmail.com>
2021-02-07 00:00:14 +0100

    some more refactor

---
+++ app/calculator/antifighter.py
@@ -0,0 +1,91 @@
+import random
+import app.calculator.assign as assign
+import app.calculator.faction_abilities as faction_abilities
+
+
+def antifighter_roll(val, swa2, jolnar_commander):
+    x = random.randint(1, 10)
+    swa2_hits = 0
+
+    # Strike Wing Alpha II destroying infantry ability
+    if swa2 and x >= 9:
+        swa2_hits = 1
+
+    if x >= val:
+        return 1, swa2_hits
+
+    # Jol-Nar commander
+    elif jolnar_commander:
+        x = random.randint(1, 10)
+
+        # Strike Wing Alpha II destroying infantry ability
+        if swa2 and x >= 9:
+            swa2_hits = 1
+
+        if x >= val:
+            return 1, swa2_hits
+
+    return 0, swa2_hits
+
+
+def generate_antifighter_hits(units, swa2, options, attacker):
+    jolnar_commander = (attacker and options["att_jolnar_commander"]) or \
+                       (not attacker and options["def_jolnar_commander"])
+    result = 0
+    swa2_hits = 0
+    best_dice = 11
+    best_unit_destroyer = False
+    for u in units:
+        for val in u.afb:
+            if val < best_dice:
+                best_dice = val
+                best_unit_destroyer = u.name == "destroyer"
+            best_dice = min(best_dice, val)
+            roll, swa2_roll = antifighter_roll(val, swa2 and u.name == "destroyer", jolnar_commander)
+            result += roll
+            swa2_hits += swa2_roll
+
+    # Argent Commander
+    if (attacker and options["att_argent_commander"]) or (not attacker and options["def_argent_commander"]):
+        roll, swa2_roll = antifighter_roll(best_dice, swa2 and best_unit_destroyer, jolnar_commander)
+        result += roll
+        swa2_hits += swa2_roll
+
+    return result, swa2_hits
+
+
+def antifighter(att_units, def_units, options):
+    att_afb_hits, att_swa2_infantry_hits = generate_antifighter_hits(att_units, options["att_faction"] == "Argent"
+                                                                     and options["att_destroyer2"], options,
+                                                                     attacker=True)
+    def_afb_hits, def_swa2_infantry_hits = generate_antifighter_hits(def_units, options["def_faction"] == "Argent"
+                                                                     and options["att_destroyer2"], options,
+                                                                     attacker=False)
+
+    # Strike Wing Alpha II infantry hits
+    if att_swa2_infantry_hits > 0:
+        def_units = faction_abilities.assign_swa2(def_units, att_swa2_infantry_hits)
+    if def_swa2_infantry_hits > 0:
+        att_units = faction_abilities.assign_swa2(att_units, def_swa2_infantry_hits)
+
+    # Argent Flight Raid Formation
+    if options["att_faction"] == "Argent" or options["def_faction"] == "Argent":
+        att_units, def_units = faction_abilities.raid_formation(att_units, def_units, att_afb_hits, def_afb_hits,
+                                                                options)
+
+    # Assign hits, depending on Waylay
+    if options["def_waylay"]:
+        att_units, options = assign.assign_hits(att_units, def_afb_hits, options["att_riskdirecthit"],
+                                                options["att_faction"],
+                                                options, True)
+    else:
+        att_units = assign.assign_fighters_only(att_units, def_afb_hits)
+
+    if options["att_waylay"]:
+        def_units, options = assign.assign_hits(def_units, att_afb_hits, options["def_riskdirecthit"],
+                                                options["def_faction"],
+                                                options, False)
+    else:
+        def_units = assign.assign_fighters_only(def_units, att_afb_hits)
+
+    return att_units, def_units, options
--- app/calculator/calculator.py
+++ app/calculator/calculator.py
@@ -7,11 +7,12 @@ import app.calculator.tech_abilities as tech_abilities
 import app.calculator.parser as parser
 import app.calculator.filters as filters
 import app.calculator.space_cannon as space_cannon
+import app.calculator.antifighter as antifighter
 import app.calculator.util as util
 
 
 def harrow(def_units, harrow_bombarders, options):
-    hits = bombardment(harrow_bombarders, options)
+    hits = bombardment(harrow_bombarders, def_units, options)
     def_units, options = assign.assign_hits(def_units, hits, True, options["def_faction"], options, False)
     return def_units, options
 
@@ -88,9 +89,6 @@ def generate_hits(units, faction, bonus, prototype, fire_team, war_funding, war_
     return hits, non_fighter_hits
 
 
-
-
-
 def combat_round(att_units, def_units, first_round, options):
     # Winnu flagship
     if options["att_faction"] == "Winnu" or options["def_faction"] == "Winnu":
@@ -163,14 +161,14 @@ def combat_round(att_units, def_units, first_round, options):
         if att_hits > 0:
             def_hits += 1
 
-    att_units, options = assign.assign_hits(att_units, def_hits, options["att_riskdirecthit"], options["att_faction"], options,
-                                     True)
+    att_units, options = assign.assign_hits(att_units, def_hits, options["att_riskdirecthit"], options["att_faction"],
+                                            options, True)
     att_units, options = assign.assign_nonfighters_first(att_units, def_nonfighter_hits, options["att_riskdirecthit"],
-                                                  options["att_faction"], options, True)
-    def_units, options = assign.assign_hits(def_units, att_hits, options["def_riskdirecthit"], options["def_faction"], options,
-                                     False)
+                                                         options["att_faction"], options, True)
+    def_units, options = assign.assign_hits(def_units, att_hits, options["def_riskdirecthit"], options["def_faction"],
+                                            options, False)
     def_units, options = assign.assign_nonfighters_first(def_units, att_nonfighter_hits, options["def_riskdirecthit"],
-                                                  options["def_faction"], options, False)
+                                                         options["def_faction"], options, False)
 
     # Duranium Armor
     if options["att_duranium"]:
@@ -219,102 +217,14 @@ def bombardment(att_units, def_units, options):
 
     if not options["att_x89"]:
         def_units, options = assign.assign_hits(def_units, bombard_hits, options["def_riskdirecthit"],
-                                         options["def_faction"],
-                                         options, False)
+                                                options["def_faction"],
+                                                options, False)
     else:
         def_units = tech_abilities.x89(def_units, bombard_hits)
 
     att_units, harrow_bombarders = filters.filter_bombardment(att_units, options["att_faction"])
 
     return att_units, def_units, harrow_bombarders, options
-
-
-def antifighter_roll(val, swa2, jolnar_commander):
-    x = random.randint(1, 10)
-    swa2_hits = 0
-
-    # Strike Wing Alpha II destroying infantry ability
-    if swa2 and x >= 9:
-        swa2_hits = 1
-
-    if x >= val:
-        return 1, swa2_hits
-
-    # Jol-Nar commander
-    elif jolnar_commander:
-        x = random.randint(1, 10)
-
-        # Strike Wing Alpha II destroying infantry ability
-        if swa2 and x >= 9:
-            swa2_hits = 1
-
-        if x >= val:
-            return 1, swa2_hits
-
-    return 0, swa2_hits
-
-
-def generate_antifighter_hits(units, swa2, options, attacker):
-    jolnar_commander = (attacker and options["att_jolnar_commander"]) or \
-                       (not attacker and options["def_jolnar_commander"])
-    result = 0
-    swa2_hits = 0
-    best_dice = 11
-    best_unit_destroyer = False
-    for u in units:
-        for val in u.afb:
-            if val < best_dice:
-                best_dice = val
-                best_unit_destroyer = u.name == "destroyer"
-            best_dice = min(best_dice, val)
-            roll, swa2_roll = antifighter_roll(val, swa2 and u.name == "destroyer", jolnar_commander)
-            result += roll
-            swa2_hits += swa2_roll
-
-    # Argent Commander
-    if (attacker and options["att_argent_commander"]) or (not attacker and options["def_argent_commander"]):
-        roll, swa2_roll = antifighter_roll(best_dice, swa2 and best_unit_destroyer, jolnar_commander)
-        result += roll
-        swa2_hits += swa2_roll
-
-    return result, swa2_hits
-
-
-def antifighter(att_units, def_units, options):
-    att_afb_hits, att_swa2_infantry_hits = generate_antifighter_hits(att_units, options["att_faction"] == "Argent"
-                                                                     and options["att_destroyer2"], options,
-                                                                     attacker=True)
-    def_afb_hits, def_swa2_infantry_hits = generate_antifighter_hits(def_units, options["def_faction"] == "Argent"
-                                                                     and options["att_destroyer2"], options,
-                                                                     attacker=False)
-
-    # Strike Wing Alpha II infantry hits
-    if att_swa2_infantry_hits > 0:
-        def_units = faction_abilities.assign_swa2(def_units, att_swa2_infantry_hits)
-    if def_swa2_infantry_hits > 0:
-        att_units = faction_abilities.assign_swa2(att_units, def_swa2_infantry_hits)
-
-    # Argent Flight Raid Formation
-    if options["att_faction"] == "Argent" or options["def_faction"] == "Argent":
-        att_units, def_units = faction_abilities.raid_formation(att_units, def_units, att_afb_hits, def_afb_hits,
-                                                                options)
-
-    # Assign hits, depending on Waylay
-    if options["def_waylay"]:
-        att_units, options = assign.assign_hits(att_units, def_afb_hits, options["att_riskdirecthit"],
-                                         options["att_faction"],
-                                         options, True)
-    else:
-        att_units = assign.assign_fighters_only(att_units, def_afb_hits)
-
-    if options["att_waylay"]:
-        def_units, options = assign.assign_hits(def_units, att_afb_hits, options["def_riskdirecthit"],
-                                         options["def_faction"],
-                                         options, False)
-    else:
-        def_units = assign.assign_fighters_only(def_units, att_afb_hits)
-
-    return att_units, def_units, options
 
 
 def start_of_space_combat(att_units, def_units, options):
@@ -345,10 +255,10 @@ def iteration(att_units, def_units, options):
         att_units, def_units, options = start_of_space_combat(att_units, def_units, options)
 
         # anti-fighter barrage
-        att_units, def_units, options = antifighter(att_units, def_units, options)
+        att_units, def_units, options = antifighter.antifighter(att_units, def_units, options)
 
-    # bombardment
     if options["ground_combat"]:
+        # bombardment
         att_units, def_units, harrow_bombarders, options = bombardment(att_units, def_units, options)
 
         # Mentak mech
@@ -523,6 +433,7 @@ def run_simulation(att_units, def_units, options, it):
 
 def calculate(attacker_units, defender_units, options, test=True):
     it = 10000 if test else 3000
+
     att_units = parser.parse_units(attacker_units, attacker=True, options=options)
     def_units = parser.parse_units(defender_units, attacker=False, options=options)
 
