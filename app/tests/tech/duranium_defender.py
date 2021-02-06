136d357fbfe765ed9927083a7043a747d9bd8f67
Erik Baalhuis <ebbaalhuis@gmail.com>
2021-02-06 15:38:43 +0100

    test for Duranium Armor (defender)

---
+++ app/tests/tech/duranium_defender.py
@@ -0,0 +1,27 @@
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '../../..'))
+from app import calculator
+from app import testing_helpers
+
+
+attacker, defender, options, tol = testing_helpers.defaults()
+
+# target source: http://alphamou.se/ti4calc/
+target = [21, 32, 47]  # target percentages; [tie, attacker, defender]
+print("1 Dread (Duranium) vs 1 Dread")
+
+# Units
+attacker["dread"] = 1
+defender["dread"] = 1
+
+# Factions
+
+# Ground Combat
+options["ground_combat"] = False
+
+# Options
+options["def_duranium"] = True
+
+outcomes = calculator.calculate(attacker, defender, options)
+testing_helpers.evaluate(outcomes, target, tol)
